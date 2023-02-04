9480ce7da0a4c4605192686699d9aeb555ba85f6
Daniel <DanielWu@duck.com>
2023-02-04 18:46:21 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,21 @@
 import whisper
 
-def audio_to_text(model_size, audio_file):
-    model = whisper.load_model(model_size)
+model = whisper.load_model("base")
 
-    # load audio and pad/trim it to fit 30 seconds
-    audio = whisper.load_audio(audio_file)
-    audio = whisper.pad_or_trim(audio)
+# load audio and pad/trim it to fit 30 seconds
+audio = whisper.load_audio("audio.mp3")
+audio = whisper.pad_or_trim(audio)
 
-    # make log-Mel spectrogram and move to the same device as the model
-    mel = whisper.log_mel_spectrogram(audio).to(model.device)
+# make log-Mel spectrogram and move to the same device as the model
+mel = whisper.log_mel_spectrogram(audio).to(model.device)
 
-    # detect the spoken language
-    _, probs = model.detect_language(mel)
-    print(f"Detected language: {max(probs, key=probs.get)}")
+# detect the spoken language
+_, probs = model.detect_language(mel)
+print(f"Detected language: {max(probs, key=probs.get)}")
 
-    # decode the audio
-    options = whisper.DecodingOptions(fp16=False)
-    result = whisper.decode(model, mel, options)
+# decode the audio
+options = whisper.DecodingOptions()
+result = whisper.decode(model, mel, options)
 
-    # print the recognized text
-    print(result.text)
-    # with open(output_file, "w") as f:
-    #     f.write(result.text)
-
-audio_to_text('tiny.en','audio.mp3')
-
-
+# print the recognized text
+print(result.text)
