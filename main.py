91a814072542147335a1941458f54342f2bada93
Daniel <DanielWu@duck.com>
2023-06-05 02:41:21 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
-import whisperx
+import whisperx, os
+
+YOUR_HF_TOKEN = os.environ['BARK_TOKEN']
 device = "cuda" 
 min_speakers = 2
 max_speakers = 2
+
 audio_file = "amzn-captcha-modal.mp3"
 diarize_model = whisperx.DiarizationPipeline(use_auth_token=YOUR_HF_TOKEN, device=device, min_speakers=min_speakers, max_speakers=max_speakers)
 
 # add min/max number of speakers if known
 diarize_segments = diarize_model(audio_file)
 # diarize_model(audio_file, min_speakers=min_speakers, max_speakers=max_speakers)
-
 result = whisperx.assign_word_speakers(diarize_segments, result)
 print(diarize_segments)
 print(result["segments"]) # segments are now assigned speaker IDs
