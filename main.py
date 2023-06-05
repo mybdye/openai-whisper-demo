74d05a8623772e630046885fdeecff77449e3403
Daniel <DanielWu@duck.com>
2023-06-05 13:14:48 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ YOUR_HF_TOKEN = os.environ['YOUR_HF_TOKEN']
 device = "cuda" 
 
 audio_file = "amzn-captcha-modal.mp3"
-diarize_model = whisperx.DiarizationPipeline(use_auth_token=YOUR_HF_TOKEN, device=device, num_speakers=2)
+diarize_model = whisperx.DiarizationPipeline(use_auth_token=YOUR_HF_TOKEN, device=device)
 
 # add min/max number of speakers if known
-diarize_segments = diarize_model(audio_file)
+diarize_segments = diarize_model(audio_file, num_speakers=2)
 # diarize_model(audio_file, min_speakers=min_speakers, max_speakers=max_speakers)
 result = whisperx.assign_word_speakers(diarize_segments, result)
 print(diarize_segments)
